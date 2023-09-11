151809004076c967d1b9f9fcbac863f32876c191
Nivadhini <nivadhini.moodley@mail.utoronto.ca>
2023-09-11 13:35:19 -0400

    Added Reduce.java

---
+++ src/Reduce.java
@@ -0,0 +1,24 @@
+public class Reduce {
+    public static void main(String[] args) {
+
+        int n = 100;
+        int counter = 0;
+
+        while (n != 0) {
+
+            if (n % 2 == 0) {
+
+                n = n / 2;
+            }
+
+            else {
+
+                n -= 1;
+            }
+
+            counter += 1;
+        }
+
+        System.out.println(counter);
+    }
+}
