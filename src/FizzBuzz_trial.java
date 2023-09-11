ab38b299489206e649887318445a2e9c120b170b
Nivadhini Moodley <nivadhinimoodley@Nivadhinis-MacBook-Pro-2.local>
2023-09-11 12:21:12 -0400

    refactored to extract helper method

--- src/FizzBuzz_trial.java
+++ src/FizzBuzz_trial.java
@@ -1,15 +1,15 @@
 /**
  * Solve the FizzBuzz challenge.
  */
-class FizzBuzz {
+class FizzBuzz_trial {
 
     public static void main(String[] args) {
         int i = 1;
 
-        while (i < 100) {
+         while (i < 100) {
 
-            i = doFizzBuzz(i);
-        }
+             i = doFizzBuzz(i);
+         }
     }
 
     private static int doFizzBuzz(int i) {
@@ -35,6 +35,7 @@ class FizzBuzz {
             System.out.println(i);
 
         }
+
         i += 1;
         return i;
     }
