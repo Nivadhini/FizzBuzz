16d3194bafecd3d4ee8a1002de5224d2f0b2bb55
Nivadhini <nivadhini.moodley@mail.utoronto.ca>
2023-09-12 19:49:41 -0400

    Updated Multiples and README

--- src/Multiples.java
+++ src/Multiples.java
@@ -5,10 +5,7 @@ public class Multiples {
 
         for (int i = 1; i < 1000; i++) {
 
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            if (divisibleBy3 || divisibleBy5) {
+            if (i % 3 == 0 || i % 5 == 0) {
 
                 n += 1;
             }
